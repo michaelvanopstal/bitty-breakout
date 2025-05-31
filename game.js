899521e40654848a1a7fa255d28f3170f6cfa6b2
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-05-31 13:55:20 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ let rightPressed = false;
 let leftPressed = false;
 
 const brickRowCount = 6;
-const brickColumnCount = 8;
+const brickColumnCount = 10;
 const brickWidth = 31;
 const brickHeight = 25;
 
