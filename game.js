84c02f0d05f9ec7882b2f27ba0cbca2f3083328d
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-05-31 23:44:39 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 let ballRadius = 8;
-let dx = 0;
+let dx = 4;
 let dy = -4;
 let ballLaunched = false;
 let x;
@@ -32,19 +32,8 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 document.addEventListener("click", () => {
-  if (!ballLaunched) {
-    dx = 0;
-    dy = -4;
-    ballLaunched = true;
-  }
+  if (!ballLaunched) ballLaunched = true;
 });
-document.addEventListener("keydown", (e) => {
-  if ((e.key === "ArrowUp" || e.key === "Up") && !ballLaunched) {
-    dx = 0;
-    dy = -4;
-    ballLaunched = true;
-  }
-}););
 document.addEventListener("keydown", (e) => {
   if ((e.key === "ArrowUp" || e.key === "Up") && !ballLaunched) ballLaunched = true;
 });
@@ -133,7 +122,7 @@ function draw() {
   drawPaddle();
   drawScore();
 
-  let steps = 12;
+  let steps = 4;
   for (let i = 0; i < steps; i++) {
     if (ballLaunched) {
       x += dx / steps;
