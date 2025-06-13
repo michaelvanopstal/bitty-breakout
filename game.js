244250565a4db1f7220dcd15075017cc12714694
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-13 20:52:33 +0200

    Update game.js

--- game.js
+++ game.js
@@ -631,7 +631,7 @@ function drawWaterBackground() {
   ctx.restore();
 }function drawWaterOverlay() {
   let waterWobble = Math.sin(Date.now() / 200) * 4; // zelfde als in background
-  const overlayHeight = 30; // hoogte van het bovenste waterstuk
+  const overlayHeight = 50; // hoogte van het bovenste waterstuk
   const visualOverlayOffset = -6; // 👈
 
   ctx.save();
