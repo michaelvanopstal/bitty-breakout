2a7d6e3015fe0dce4c36943706b2836b615f4a0a
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-14 09:47:35 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -286,7 +286,7 @@ function resetBall() {
 
 function resetPaddle() {
   paddleX = (canvas.width - paddleWidth) / 2;
-
+  boatSpeedFactor = 1; // ← ✅ ook hier terugzetten
 }
 
 
@@ -622,28 +622,15 @@ function drawWaves() {
 
 
 function drawWaterBackground() {
-  const waveHeight = 10;
-  const waveLength = 100;
-  const waveSpeed = 0.03;
-
+  let waterWobble = Math.sin(Date.now() / 200) * 4;
+  ctx.save();
   ctx.beginPath();
-  ctx.moveTo(0, currentWaterHeight);
+  ctx.rect(0, currentWaterHeight + waterWobble, canvas.width, canvas.height - (currentWaterHeight + waterWobble));
+  ctx.clip();
+  ctx.drawImage(waterBg, 0, currentWaterHeight + waterWobble, canvas.width, canvas.height - currentWaterHeight);
+  ctx.restore();
 
-  for (let x = 0; x <= canvas.width; x++) {
-    let y = currentWaterHeight + Math.sin(x * 0.05 + Date.now() * waveSpeed) * waveHeight;
-    ctx.lineTo(x, y);
-  }
-
-  ctx.lineTo(canvas.width, canvas.height);
-  ctx.lineTo(0, canvas.height);
-  ctx.closePath();
-
-  ctx.fillStyle = "rgba(0, 119, 190, 0.4)"; // semi-transparant blauw water
-  ctx.fill();
-}
-
-
- function drawWaterOverlay() {
+}function drawWaterOverlay() {
   let waterWobble = Math.sin(Date.now() / 200) * 4; // zelfde als in background
   const overlayHeight = 80; // hoogte van het bovenste waterstuk
   const visualOverlayOffset = -20; // 👈
@@ -660,7 +647,7 @@ function drawWaterBackground() {
     currentWaterHeight + waterWobble,  // waar het water is
     canvas.width,
     overlayHeight
- 
+  );
   ctx.restore();
 }
 
@@ -675,14 +662,9 @@ function draw() {
 
   // 🌊 Water tijdens bootmodus
   if (boatPhase !== "inactive") {
+    drawWaves();
     drawWaterBackground();
   }
-
-  if (boatPhase === "rising") {
-  currentWaterHeight -= 0.5;
-} else if (boatPhase === "falling") {
-  currentWaterHeight += 0.5;
-}
 
   drawPaddle();
   drawWaterOverlay();
