a74a83d7f3ab93e0604c350f56b473efd1f7d284
vkjhbjk <44727975+michaelvanopstal@users.noreply.github.com>
2025-10-25 17:54:21 +0200

    Update game.js

--- game.js
+++ game.js
@@ -2566,7 +2566,7 @@ function onImageLoad() {
   if (imagesLoaded === 27) {
     // Zodra alle afbeeldingen klaar zijn:
     updateLivesDisplay();    // laat levens zien
-    goToLevel(5, {           // direct starten op level 4
+    goToLevel(6, {           // direct starten op level 4
       resetScore: true,
       resetLives: true,
       centerPaddle: true
