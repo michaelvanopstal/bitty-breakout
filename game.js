234ef334ccca2f4a3f6e238fe48af770a49d25ad
vkjhbjk <44727975+michaelvanopstal@users.noreply.github.com>
2025-11-15 06:43:01 +0100

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const scaleFactor = currentScale;                    // dan pas scaleFactor
 
 // Paddle-ruimte onderkant — mobiel = lager, desktop = hoger
 const PADDLE_MARGIN_BOTTOM =
-  (window.innerWidth <= 768 ? 0 * scaleFactor : 8 * scaleFactor);
+  (window.innerWidth <= 1200 ? 0 * scaleFactor : 8 * scaleFactor);
 // ≤ 768px = mobiele layout
 // > 768px  = desktop layout
 
